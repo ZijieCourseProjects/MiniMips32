02cd792227a87448cdbb59130441ba289c2b69e2
shekinah_z <479404627@qq.com>
2022-10-08 09:15:44 +0800

    增加了数据移动指令

---
+++ TEMU/pytemu/src/instructions/r_type_ins/mflo_ins.py
@@ -0,0 +1,15 @@
+import instructions.r_type_ins.RIns as RIns
+from RegList import RegList
+from ctypes import *
+
+
+class mflo_ins(RIns.R_Ins):
+    def __init__(self, instruction):
+        super().__init__(instruction)
+
+    def execute(self, cpu):
+        # LO = 34
+        cpu[self._rd].low32 = c_int32(cpu[34].low32).value
+
+    def __str__(self):
+        return f"mflo ${RegList(self._rd).name}"
---
+++ TEMU/pytemu/src/instructions/r_type_ins/mthi_ins.py
@@ -0,0 +1,15 @@
+import instructions.r_type_ins.RIns as RIns
+from RegList import RegList
+from ctypes import *
+
+
+class mthi_ins(RIns.R_Ins):
+    def __init__(self, instruction):
+        super().__init__(instruction)
+
+    def execute(self, cpu):
+        # HI = 33
+        cpu[33].low32 = c_int32(cpu[self._rs].low32).value
+
+    def __str__(self):
+        return f"mthi ${RegList(self._rs).name}"
