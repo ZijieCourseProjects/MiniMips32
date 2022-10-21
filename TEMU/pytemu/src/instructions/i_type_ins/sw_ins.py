1017f02da8ec14b8c9a91ec7bbc990d63fc95894
shekinah_z <479404627@qq.com>
2022-10-21 21:22:47 +0800

    增加了syscall指令与访存指令中对异常的处理

--- TEMU/pytemu/src/instructions/i_type_ins/sw_ins.py
+++ TEMU/pytemu/src/instructions/i_type_ins/sw_ins.py
@@ -1,6 +1,7 @@
 from instructions.i_type_ins.IIns import I_Ins
 from RegList import RegList
 from instructions.Instruction import signed_extend
+from ExcCode import ExcCode
 
 
 class sw_ins(I_Ins):
@@ -9,7 +10,10 @@ class sw_ins(I_Ins):
 
     def execute(self, cpu):
         addr = cpu[self._rs].low32 + signed_extend(self._imm, 16)
-        cpu.mem.write(addr, cpu[self._rt].low32, 4)
+        if addr % 4 == 0:
+            cpu.mem.write(addr, cpu[self._rt].low32, 4)
+        else:
+            cpu.raise_exption(ExcCode.ADES)
 
     def __str__(self):
         return f"sw {RegList(self._rt).name}, {self._imm}({RegList(self._rs).name})"
