efe7fab78b97ac5fd1854173ab317ebe02932a84
doriko <1626744889@qq.com>
2022-11-15 14:43:55 +0800

    修改了暂停阶段的if_stage和ifid_reg

--- MiniMIPS32/MiniMIPS32.srcs/sources_1/new/ifid_reg.v
+++ MiniMIPS32/MiniMIPS32.srcs/sources_1/new/ifid_reg.v
@@ -7,6 +7,8 @@ module ifid_reg (
 	input  wire [`INST_ADDR_BUS]       if_pc,
 	input  wire [`INST_ADDR_BUS]       if_pc_plus_4,
 
+    input  wire  [`STALL_BUS]           stall,
+    
 	output reg  [`INST_ADDR_BUS]       id_pc,
 	output reg  [`INST_ADDR_BUS]       id_pc_plus_4
 	);
@@ -16,8 +18,12 @@ module ifid_reg (
 			id_pc 	<= `PC_INIT;
 			id_pc_plus_4 <= `ZERO_WORD;
 		end
-		else begin
-			id_pc	<= if_pc;
+		else if(stall[1]==`STOP&&stall[2]==`NOSTOP) beign
+		 id_pc    <= `ZERO_WORD;        
+         id_pc_plus_4 <= `ZERO_WORD;
+		end
+		else if(stall[1]==`NOSTOP)begin
+			id_pc	     <= if_pc;
 			id_pc_plus_4 <= if_pc_plus_4;
 		end
 	end
